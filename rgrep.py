01adf92a241bd07b3ff62a9a7f262856d29ad523
stevenbuccini <steven@stevenbuccini.com>
2013-03-20 01:41:54 -0700

    Improved documentation

--- rgrep.py
+++ rgrep.py
@@ -9,24 +9,16 @@ def match_asterisk(tosearch, pattern, escaped=False):
 		@param escaped: True if the character has no special value because it's been escaped.
 
 	"""
-	#if pattern string is empty, then we've matched all its characters.  Return True.
-	if len(pattern) == 0:
+	if len(pattern) == 0: #pattern found in search string
 		return True
-
-	#if the text to be pattern is empty, but the pattern string is not, then we
-	#didn't find our pattern.  Return False.
-	if len(tosearch) == 0:
+	if len(tosearch) == 0: #pattern not found in search string.
 		return False
 
-	#if current character is a dot and hasn't been escaped, we try to match it and see if
-	#expanding the string yields a match as well.
-	if pattern[0] == "." and  (not escaped):
+	if pattern[0] == "." and  (not escaped): #checks case where no char or extra char appears
 		return match_asterisk(tosearch[1:], pattern) or control(tosearch[1:], pattern[2:])
-	#if current character is normal, try to match, then go deeper.
-	elif pattern[0] == tosearch[0]:
+	elif pattern[0] == tosearch[0]: #if current character is normal, try to match, then go deeper.
 		return match_asterisk(tosearch[1:], pattern, True) or control(tosearch[1:], pattern[2:])
-	#neither case works, give command back to the control function
-	else:
+	else: 
 		return control(tosearch, pattern[2:])
 
 def control(tosearch, pattern):
@@ -36,46 +28,35 @@ def control(tosearch, pattern):
 		then depending on its value goes to the appropriate matching case.
 	
 	"""
-
-	# We've matched all characters in the pattern, or the other character in the pattern doesn't have 
-	#to appear so return true.
-	if pattern == "":
+	if pattern == "": #all characters have been matched.
 		return True
-	# The pattern substring wasn't found in the line, so return false.
-	if tosearch == "":
+	if tosearch == "": # The pattern substring wasn't found in the line, so return false.
 		if len(pattern) >= 2 and (pattern[0] != "\\" and pattern[1] == "*"):
-			return True		
+			return True	#We look ahead to ensure that an asterisk doesn't affect results.
 		return False
-
-	# Handles escape character, including looking ahead to see if asterisk applies to escaped character
-	if pattern[0] == "\\":
+	if pattern[0] == "\\": #handles escape character
 		if len(pattern) > 2:
-			if pattern[2] == "*":
+			if pattern[2] == "*": #looks ahead to see if asterisk will apply
 				return match_asterisk(tosearch, pattern[1:], True)
 		else:
-			# If not special case, match characters naively
-			if tosearch[0] == pattern[1]:
+			if tosearch[0] == pattern[1]: # If not special case, match characters naively
 				return control(tosearch[1:], pattern[2:])
 			else:
 				False
 
-	# Handles the asterisk special case.
-	elif len(pattern) > 1 and pattern[1] == "*":
+	elif len(pattern) > 1 and pattern[1] == "*": # Handles the asterisk special case.
 		if len(pattern) > 1:
 			return control(tosearch, pattern[2:]) or match_asterisk(tosearch, pattern)
 		else:
 			return control(tosearch, "")
-	# Handles the dot special case.  The empty string will be caught before this.
-	elif  pattern[0] == ".":
+
+	elif  pattern[0] == ".": # Handles the dot special case.  The empty string will be caught before this.
 		return control(tosearch[1:], pattern[1:])
-	# Matches normal characters
-	elif pattern[0] == tosearch[0]:
+
+	elif pattern[0] == tosearch[0]:# Matches normal characters
 		return control(tosearch[1:], pattern[1:])
 	else:
 		return False
-
-
-
 
 def rgrep_matches(input_file, pattern):
 	""" A restriced implementation of grep. Checks if pattern (type: string) is
@@ -97,13 +78,9 @@ def rgrep_matches(input_file, pattern):
 		All other characters have no special values
 
 	"""
-
-
-
 	text_file = open(input_file, 'r') #input source file.
 	tosearch = text_file.readline() #grab the first line
 
-	
 	while (tosearch != ""): #checks each line in text file.
 		# Starts search pattern at next char in search line
 		for i in range(len(tosearch)):
@@ -112,7 +89,7 @@ def rgrep_matches(input_file, pattern):
 		tosearch = text_file.readline()
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": #handles arguments from command line
 	input_file = sys.argv[1]
 	pattern = sys.argv[2]
 	rgrep_matches(input_file, pattern)
